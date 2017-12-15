35e4a2aa6ac47d4eb02d6127aa1e3da484d650ea
bordia98 <bhavyabordia@gmail.com>
2017-12-16 03:08:15 +0530

    full screen bug resolved

--- app/src/main/java/com/example/bordia98/shortcut/splashscreen.java
+++ app/src/main/java/com/example/bordia98/shortcut/splashscreen.java
@@ -3,12 +3,16 @@ package com.example.bordia98.shortcut;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class splashscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashscreenoriginal);
 
         Thread th = new Thread(){
